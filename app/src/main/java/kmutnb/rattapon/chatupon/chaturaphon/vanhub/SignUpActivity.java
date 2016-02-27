65274baa0a2ca10ee70ea7f1bc15e4768a511d14
ECT <min93@live.co.uk>
2016-02-27 14:35:06 +0700

    Update SQL Success

--- app/src/main/java/kmutnb/rattapon/chatupon/chaturaphon/vanhub/SignUpActivity.java
+++ app/src/main/java/kmutnb/rattapon/chatupon/chaturaphon/vanhub/SignUpActivity.java
@@ -1,12 +1,25 @@
 package kmutnb.rattapon.chatupon.chaturaphon.vanhub;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.os.StrictMode;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.Toast;
+
+import org.apache.http.NameValuePair;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
+
+import java.util.ArrayList;
 
 public class SignUpActivity extends AppCompatActivity {
 
@@ -134,8 +147,84 @@ public class SignUpActivity extends AppCompatActivity {
 
         } else {
 
+            //No Space
+            confirmData();
+
         } // if
 
     }   // clickSaveData
+
+    private void confirmData() {
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setIcon(R.drawable.icon_myaccount);
+        builder.setTitle("โปรดตรวจสอบข้อมูล");
+        builder.setMessage(getResources().getString(R.string.user) + nameString + "\n" +
+                "User = " + userString + "\n" +
+                getResources().getString(R.string.pass) + passwordString + "\n" +
+                getResources().getString(R.string.email) + emailString + "\n" +
+                getResources().getString(R.string.tel) + phoneString + "\n" +
+                getResources().getString(R.string.stop) + stopString + "\n" +
+                getResources().getString(R.string.price) + priceString + "\n" +
+                getResources().getString(R.string.timeStart) + timeStartString + "\n" +
+                getResources().getString(R.string.timeEnd) + timeEndString + "\n" +
+                getResources().getString(R.string.specialnews) + newsString);
+        builder.setCancelable(false);
+        builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                updateToMySQL();
+                dialog.dismiss();
+            }
+        });
+        builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+
+                dialog.dismiss();
+
+            }
+        });
+        builder.show();
+
+    }   // confirmData
+
+    private void updateToMySQL() {
+
+        //Change Policy
+        StrictMode.ThreadPolicy threadPolicy = new StrictMode.ThreadPolicy
+                .Builder().permitAll().build();
+        StrictMode.setThreadPolicy(threadPolicy);
+
+        try {
+
+            ArrayList<NameValuePair> nameValuePairs = new ArrayList<NameValuePair>();
+            nameValuePairs.add(new BasicNameValuePair("isAdd", "true"));
+            nameValuePairs.add(new BasicNameValuePair("Name", nameString));
+            nameValuePairs.add(new BasicNameValuePair("User", userString));
+            nameValuePairs.add(new BasicNameValuePair("Password", passwordString));
+            nameValuePairs.add(new BasicNameValuePair("Email", emailString));
+            nameValuePairs.add(new BasicNameValuePair("Phone", phoneString));
+            nameValuePairs.add(new BasicNameValuePair("Lat", "123"));
+            nameValuePairs.add(new BasicNameValuePair("Lng", "456"));
+            nameValuePairs.add(new BasicNameValuePair("Stop", stopString));
+            nameValuePairs.add(new BasicNameValuePair("Price", priceString));
+            nameValuePairs.add(new BasicNameValuePair("timeStart", timeStartString));
+            nameValuePairs.add(new BasicNameValuePair("timeEnd", timeEndString));
+            nameValuePairs.add(new BasicNameValuePair("News", newsString));
+
+            HttpClient httpClient = new DefaultHttpClient();
+            HttpPost httpPost = new HttpPost("http://swiftcodingthai.com/van/php_add_user_m.php");
+            httpPost.setEntity(new UrlEncodedFormEntity(nameValuePairs, "UTF-8"));
+            httpClient.execute(httpPost);
+
+            Toast.makeText(SignUpActivity.this, "บันทึกข้อมูลเรียบร้อย", Toast.LENGTH_SHORT).show();
+            finish();
+
+        }catch (Exception e) {
+            Toast.makeText(SignUpActivity.this, "ม่สามาถรอัพข้อมูลได้", Toast.LENGTH_SHORT).show();
+        }
+
+    }   // updateToMySQL
 
 }   // Main Class
